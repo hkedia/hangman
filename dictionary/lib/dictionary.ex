26173aa55ebcc601affb506cd26d124227f04cff
Harsh Kedia <hkedia@hey.com>
2024-06-09 14:49:38 +0530

    start dictionary automatically as an application

--- dictionary/lib/dictionary.ex
+++ dictionary/lib/dictionary.ex
@@ -1,10 +1,6 @@
 defmodule Dictionary do
-  alias Dictionary.Impl.WordList
-  @opaque t :: WordList.t
+  alias Dictionary.Runtime.Server
 
-  @spec start() :: t
-  defdelegate start, to: WordList, as: :word_list
-
-  @spec random_word(t) :: String.t
-  defdelegate random_word(word_list), to: WordList
+  @spec random_word() :: String.t()
+  defdelegate random_word(), to: Server
 end
